57b268f7c39d11237b4081daee560785047f430b
Sasha7b9Work <Phuntik-it@tut.by>
2023-03-21 11:53:49 +0300

    *

---
+++ sources/common/_VS_LIBS/STM32F1/stm32f1xx_hal_adc_ex.cpp
@@ -0,0 +1,7 @@
+// 2023/03/21 11:47:45 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include <stm32f1xx_hal.h>
+
+HAL_StatusTypeDef HAL_ADCEx_Calibration_Start(ADC_HandleTypeDef *)
+{
+    return HAL_OK;
+}
