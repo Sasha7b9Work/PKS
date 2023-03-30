7a63a17844c0f442878c83ea5f96fd3214be3ad1
Sasha7b9Work <Phuntik-it@tut.by>
2023-03-30 11:16:48 +0300

    *

--- sources/TestHardware/src/Hardware/Modules/SIM800C/SIM800C.cpp
+++ sources/TestHardware/src/Hardware/Modules/SIM800C/SIM800C.cpp
@@ -1,6 +1,6 @@
 // 2023/03/30 10:59:50 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
-#include "Modem/SIM800C.h"
+#include "Hardware/Modules/SIM800C/SIM800C.h"
 
 
 void SIM800C::Init()
