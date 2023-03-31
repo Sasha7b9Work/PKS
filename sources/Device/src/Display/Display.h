52c6e52f5ee401568215119c37f91b6d2ed6b214
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2023-03-31 20:50:12 +0300

    *

--- sources/Device/src/Display/Display.h
+++ sources/Device/src/Display/Display.h
@@ -1,10 +1,28 @@
-// 2023/03/29 20:04:48 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+// 2023/03/21 09:58:21 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
 
-// Модуль дисплея
+struct Color
+{
+    enum E
+    {
+        Count
+    };
+
+
+    static const uint16 colors[Count];
+};
+
 
 namespace Display
 {
+    static const int WIDTH = 128;
+    static const int HEIGHT = 64;
+
     void Init();
 }
+
+
+#define BLUE_FROM_COLOR(color)  ((color) & 0x1f)
+#define GREEN_FROM_COLOR(color) (((color) >> 5) & 0x3f)
+#define RED_FROM_COLOR(color)   (((color) >> 11) & 0x1f)
---
+++ sources/VS/Device/Display/Display.cpp
@@ -0,0 +1,9 @@
+// 2023/03/31 20:46:20 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Display/Display.h"
+
+
+void Display::Init()
+{
+
+}
