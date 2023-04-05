bbf74eb46859ed9f618c9fd658f2de1e817ddc81
Sasha7b9Work <Phuntik-it@tut.by>
2023-04-05 16:04:27 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL_ROM.cpp
+++ sources/Device/src/Hardware/HAL/HAL_ROM.cpp
@@ -1,11 +1,12 @@
 ﻿// 2023/04/05 13:04:49 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Hardware/HAL/HAL.h"
+#include <gd32f30x.h>
 
 
 void HAL_ROM::EraseSector(int)
 {
-
+    fmc_unlock();
 }
 
 
