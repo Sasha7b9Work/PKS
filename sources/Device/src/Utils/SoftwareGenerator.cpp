6271fca8bdf41628bb24a6fc8592c6457188bd82
Sasha7b9Work <Phuntik-it@tut.by>
2023-04-07 11:04:23 +0300

    *

--- sources/Device/src/Utils/SoftwareGenerator.cpp
+++ sources/Device/src/Utils/SoftwareGenerator.cpp
@@ -1,4 +1,4 @@
-// 2023/04/07 10:58:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+﻿// 2023/04/07 10:58:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Utils/SoftwareGenerator.h"
 
--- sources/Device/src/Utils/SoftwareGenerator.h
+++ sources/Device/src/Utils/SoftwareGenerator.h
@@ -1,4 +1,4 @@
-// 2023/04/07 10:58:48 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+﻿// 2023/04/07 10:58:48 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 #include "Measurer/Measurer.h"
 
