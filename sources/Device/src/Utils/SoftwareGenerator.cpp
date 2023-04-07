899f84677c196ddbcbd29405ed465992f0189daa
Sasha7b9Work <Phuntik-it@tut.by>
2023-04-07 15:13:58 +0300

    Измерение на синусоиде работает

--- sources/Device/src/Utils/SoftwareGenerator.cpp
+++ sources/Device/src/Utils/SoftwareGenerator.cpp
@@ -1,26 +1,31 @@
-﻿// 2023/04/07 10:58:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+// 2023/04/07 10:58:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Utils/SoftwareGenerator.h"
+#include <cmath>
+#include <cstdlib>
+
+
+#define M_PI 3.141592653589793238462643383279502884
 
 
 namespace Generator
 {
-    static void GenerateLineVoltage(Sample[NUM_SAMPLES], float level);
-    static void GenerateLineCurrent(Sample[NUM_SAMPLES], float level);
-    static void GenerateSineVoltage(Sample[NUM_SAMPLES], float amplitude);
-    static void GenerateSineCurrent(Sample[NUM_SAMPLES], float amplitude);
+    void GenerateLineVoltage(Sample[NUM_SAMPLES], float level);
+    void GenerateLineCurrent(Sample[NUM_SAMPLES], float level);
+    void GenerateSineVoltage(Sample[NUM_SAMPLES], float amplitude);
+    void GenerateSineCurrent(Sample[NUM_SAMPLES], float amplitude);
 }
 
 
 void Generator::GenerateVoltage(Sample samples[NUM_SAMPLES])
 {
-    GenerateLineVoltage(samples, 220.0f);
+    GenerateSineVoltage(samples, 100.0);
 }
 
 
 void Generator::GenerateCurrent(Sample samples[NUM_SAMPLES])
 {
-    GenerateLineCurrent(samples, 10.0f);
+    GenerateSineCurrent(samples, 10.0);
 }
 
 
@@ -44,11 +49,25 @@ void Generator::GenerateLineCurrent(Sample samples[NUM_SAMPLES], float level)
 
 void Generator::GenerateSineVoltage(Sample samples[NUM_SAMPLES], float amplitude)
 {
+    double start_angle = (double)std::rand() / 10.0;
 
+    double dA = 2.0 * M_PI / SAMPLES_ON_PERIOD;
+
+    for (int i = 0; i < NUM_SAMPLES; i++)
+    {
+        samples[i].FromVoltage((float)((double)amplitude * std::sin(start_angle + dA * i)));
+    }
 }
 
 
 void Generator::GenerateSineCurrent(Sample samples[NUM_SAMPLES], float amplitude)
 {
+    double start_angle = (double)std::rand() / 10.0;
 
+    double dA = 2.0 * M_PI / SAMPLES_ON_PERIOD;
+
+    for (int i = 0; i < NUM_SAMPLES; i++)
+    {
+        samples[i].FromCurrent((float)(amplitude * std::sin(start_angle + dA * i)));
+    }
 }
--- sources/common/_VS_KEIL/std_includes/math.h
+++ sources/common/_VS_KEIL/std_includes/math.h
@@ -6,6 +6,7 @@ float sqrtf(float);
 float log10f(float);
 float cosf(float);
 float sinf(float);
+double sin(double);
 float expf(float);
 void srand(unsigned int);
 float powf(float, float);
