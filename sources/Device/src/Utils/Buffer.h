54f1450a41ca26301c8be6250a2839788b1d5182
Sasha7b9Work <Phuntik-it@tut.by>
2023-04-13 10:27:33 +0300

    *

--- sources/Device/src/Utils/Buffer.h
+++ sources/Device/src/Utils/Buffer.h
@@ -10,64 +10,7 @@ public:
 
     Buffer() : size(size_buffer) {}; //-V730
 
-    Buffer(int _size) : size(size_buffer) //-V730
-    {
-        Realloc(_size);
-    }
-
-    Buffer(int _size, T value) : size(size_buffer)
-    {
-        Realloc(_size);
-        Fill(value);
-    }
-
-    void Fill(T value)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            buffer[i] = value;
-        }
-    }
-
     T *Data() { return buffer; }
-
-    const T *DataConst() const { return buffer; }
-
-    void Realloc(int _size)
-    {
-        size = _size;
-
-        if (size > size_buffer)
-        {
-            size = size_buffer;
-        }
-    }
-
-    void ReallocAndFill(int _size, T value)
-    {
-        Realloc(_size);
-
-        for (int i = 0; i < size; i++)
-        {
-            buffer[i] = value;
-        }
-    }
-
-    // Перевыделить память и заполнить её из buffer
-    void ReallocFromBuffer(const T *in, int _size)
-    {
-        size = _size;
-
-        if (size > size_buffer)
-        {
-            size = size_buffer;
-        }
-
-        for (int i = 0; i < size; i++)
-        {
-            buffer[i] = in[i];
-        }
-    }
 
     T &operator[](int i)
     {
