19c93c3fb0ca72113021be4cded54aff6fb98843
Sasha7b9 <Sasha7b9@tut.by>
2023-05-05 15:07:07 +0300

    *

--- sources/Device/src/Modem/MQTT.cpp
+++ sources/Device/src/Modem/MQTT.cpp
@@ -147,7 +147,7 @@ void MQTT::Update(const String &answer)
             if (need_measure)
             {
 //                PublishPacket("base/state/voltage_c", "12.24");
-                PublishPacket("state/text", "message1");
+                PublishPacket("base/state/text", "message1");
 //                PublishPacket("base/state/voltage_c", "25235");
                 need_measure = false;
             }
