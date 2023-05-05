04e791773522738b14a26e5e08a4f25b1455c463
Sasha7b9 <Sasha7b9@tut.by>
2023-05-05 13:05:45 +0300

    *

--- sources/Device/src/Modem/MQTT.cpp
+++ sources/Device/src/Modem/MQTT.cpp
@@ -141,7 +141,7 @@ void MQTT::Update(const String &answer)
             if (need_measure)
             {
                 PublishPacket("state/text", "12345");
-//                PublishPacket("base/state/voltage_c", "25235");
+                PublishPacket("base/state/voltage_c", "25235");
                 need_measure = false;
             }
             else if(need_ping)
@@ -164,7 +164,7 @@ void MQTT::SendMeasure(const FullMeasure &meas)
 
     need_measure = true;
 
-    SIM800::Transmit("AT+CIPSEND");
+//    SIM800::Transmit("AT+CIPSEND");
 }
 
 
