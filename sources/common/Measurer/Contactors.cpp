8c62ed4787e37dc9a11023290ade325fe3c71c0a
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2023-05-23 20:21:42 +0300

    *

--- sources/common/Measurer/Contactors.cpp
+++ sources/common/Measurer/Contactors.cpp
@@ -101,6 +101,10 @@ namespace Contactors
             TRANSIT_DIS_2,
             TRANSIT_DIS_3,
             TRANSIT_DIS_4,
+#ifdef FIVE_STEPS_VERSION
+            STAGE_5_1,
+            STAGE_5_2,
+#endif
             STAGE_4_1,
             STAGE_4_2,
             STAGE_3_1,
@@ -143,12 +147,21 @@ namespace Contactors
     // Состояние контакторов
     namespace Level
     {
-        static const int LESS_190 = -4;
-        static const int TRANSIT = 0;
-        static const int ABOVE_270 = 4;
+#ifdef FOUR_STEPS_VERSION
+        static const int LESS_180 = -4;
+        static const int ABOVE_280 = 4;
+        static const int MIN = LESS_180;
+        static const int MAX = ABOVE_280;
+#endif
 
-        static const int MIN = LESS_190;
-        static const int MAX = ABOVE_270;
+#ifdef FIVE_STEPS_VERSION
+        static const int LESS_170 = -5;
+        static const int ABOVE_290 = 5;
+        static const int MIN = LESS_170;
+        static const int MAX = ABOVE_290;
+#endif
+
+        static const int TRANSIT = 0;
 
         static int current[3] = { 0, 0, 0 };
     }
@@ -161,6 +174,24 @@ namespace Contactors
 
     // Возвращаемое значение true означает, что фаза находится в режиме перелючения. Измерения по ней производить нельзя
     bool IsBusy(Phase::E phase);
+
+    // Устанавливает адрес на линиях MX
+    static void SetAddressMX(uint);
+
+    // Возвращает состояние реле, выбранного установленным ранее адресом по SetAddressMX()
+    static bool StateRele();
+
+    // Возвращает true, если реле по адресу address находится в состоянии переключения (нельзя замерять)
+    static bool ReleIsBusy(uint address);
+
+    // Сюда накапливаются состояния всех реле, чтобы потом одной строкой отослать неисправные
+    static bool state_contactor[NUM_PINS_MX] =
+    {
+        false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false,
+        false
+    };
 }
 
 
@@ -202,14 +233,8 @@ void Contactors::Update(const FullMeasure &measure)
 
 void Contactors::UpdatePhase(Phase::E phase, const PhaseMeasure &measure, bool is_good)
 {
-    //  KM8 -> KM9
-    //  KM -> KM8
-    //  KM -> KM7
-    //  KM -> KM6
-    //  KM -> KM5
-
-#define ENABLE_RELE(num, state)  { Enable(num, phase, state, meter[phase]);  break; }
-#define DISABLE_RELE(num, state) { Disable(num, phase, state, meter[phase]); break; }
+#define ENABLE_RELE(num, state)  { Enable(num, phase, state, meter[phase]); }
+#define DISABLE_RELE(num, state) { Disable(num, phase, state, meter[phase]); }
 
 #define WAIT_ENABLE_RELE(num, state)  if(meter[phase].IsWorked()) { ENABLE_RELE(num, state); }
 #define WAIT_DISABLE_RELE(num, state) if(meter[phase].IsWorked()) { DISABLE_RELE(num, state); }
@@ -266,9 +291,11 @@ void Contactors::UpdatePhase(Phase::E phase, const PhaseMeasure &measure, bool i
             ENABLE_RELE(2, State::TRANSIT_EN_1);
         }
 
-    case State::TRANSIT_EN_1:   WAIT_ENABLE_RELE(3, State::TRANSIT_EN_2);
-    case State::TRANSIT_EN_2:   WAIT_DISABLE_RELE(1, State::TRANSIT_EN_3);
-    case State::TRANSIT_EN_3:   WAIT_DISABLE_RELE(2, State::TRANSIT_EN_4);
+        break;
+
+    case State::TRANSIT_EN_1:   WAIT_ENABLE_RELE(3, State::TRANSIT_EN_2);       break;
+    case State::TRANSIT_EN_2:   WAIT_DISABLE_RELE(1, State::TRANSIT_EN_3);      break;
+    case State::TRANSIT_EN_3:   WAIT_DISABLE_RELE(2, State::TRANSIT_EN_4);      break;
     case State::TRANSIT_EN_4:       
         if (meter[phase].IsWorked()) { meter[phase].SetResponseTime(TIME_WAIT_BIG); State::current[phase] = State::TRANSIT_EN_5; } break;
 
@@ -278,7 +305,7 @@ void Contactors::UpdatePhase(Phase::E phase, const PhaseMeasure &measure, bool i
     case State::TRANSIT_EN_6:
         if (meter[phase].IsWorked())
         {
-            if (Level::current[phase] == Level::TRANSIT) { DISABLE_RELE(5, State::TRANSIT_DIS_1); }
+            if (Level::current[phase] == Level::TRANSIT)                        { DISABLE_RELE(5, State::TRANSIT_DIS_1); }
             else if (Level::current[phase] == -4 || Level::current[phase] == 4) { ENABLE_RELE(6, State::STAGE_4_1); }
             else if (Level::current[phase] == -3 || Level::current[phase] == 3) { ENABLE_RELE(6, State::STAGE_3_1); }
             else if (Level::current[phase] == -2 || Level::current[phase] == 2) { DISABLE_RELE(6, State::STAGE_2_1); }
@@ -334,6 +361,8 @@ void Contactors::UpdatePhase(Phase::E phase, const PhaseMeasure &measure, bool i
         }
         break;
     }
+
+    Modem::Send::LevelContactors(Level::current);
 }
 
 
@@ -386,4 +415,94 @@ void Contactors::Contactor::Disable()
 
         pin->Reset();
     }
+}
+
+
+void Contactors::VerifyCondition()
+{
+    static TimeMeterMS meter;
+
+    if (meter.ElapsedTime() < 1)
+    {
+        return;
+    }
+
+    meter.Reset();
+
+    static bool first = true;
+
+    static uint address = 0;
+
+    if (first)
+    {
+        SetAddressMX(address);
+        first = false;
+    }
+    else
+    {
+        if (!ReleIsBusy(address))
+        {
+            bool state = StateRele();
+
+            state_contactor[address] = state;
+        }
+
+        if (address == 27)                          // Был выставлен адрес P2 = 31
+        {
+            state_contactor[address] = !pinP2.IsHi();
+        }
+
+        address = Math::CircularIncrease(address, 0U, (uint)NUM_PINS_MX);
+
+        if (address == 27)                          // 28-й элемент массива - адрес пина P2 для контроля напряжения 100В
+        {
+            address = 31;
+        };
+
+        SetAddressMX(address);
+
+        if (address == 31)
+        {
+            address = 27;
+        }
+
+        if (address == 0)   // Опросили все реле, будем посылать результат
+        {
+            Modem::Send::StateContactors(state_contactor);
+        }
+    }
+}
+
+
+void Contactors::SetAddressMX(uint address)
+{
+    pinMX0.SetState((address & 1) != 0);
+    pinMX1.SetState((address & 2) != 0);
+    pinMX2.SetState((address & 4) != 0);
+    pinMX3.SetState((address & 8) != 0);
+    pinMX4.SetState((address & 16) == 0);
+}
+
+
+bool Contactors::StateRele()
+{
+    bool p1 = pinP1.IsHi();
+    bool p2 = pinP2.IsHi();
+
+    return ((p1 && !p2) || (!p1 && p2));
+}
+
+
+bool Contactors::ReleIsBusy(uint address)
+{
+    if (address < 9)
+    {
+        return Contactors::IsBusy(Phase::A);
+    }
+    else if (address < 18)
+    {
+        return Contactors::IsBusy(Phase::B);
+    }
+
+    return Contactors::IsBusy(Phase::C);
 }
