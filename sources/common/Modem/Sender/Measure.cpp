d056ad91fcc268d2992d9fa7b11a0b87a8c975cb
Sasha7b9Work <Phuntik-it@tut.by>
2023-05-26 11:48:01 +0300

    Уменьшены задержки на неисправный I2C@

---
+++ sources/common/Modem/Sender/Measure.cpp
@@ -0,0 +1,83 @@
+// 2023/05/26 11:43:55 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Modem/Sender/Measure.h"
+#include "Hardware/Timer.h"
+#include <cstdio>
+#include <cstring>
+
+
+namespace Sender
+{
+    namespace Measure
+    {
+        static FullMeasure value;
+        static bool need = false;
+
+        void Send(const FullMeasure &meas)
+        {
+            if (!MQTT::InStateRunning())
+            {
+                return;
+            }
+
+            static TimeMeterMS meter;
+
+            static bool first = true;
+
+            if (meter.ElapsedTime() < 60000 && !first)
+            {
+                return;
+            }
+
+            first = false;
+
+            meter.Reset();
+
+            value = meas;
+
+            need = true;
+
+            MQTT::Send::SendRequest();
+        }
+
+        void SendMeasure(pchar name, float voltage)
+        {
+            char buffer[32];
+            std::sprintf(buffer, "%d", (int)(voltage + 0.5f));
+            for (uint i = 0; i < std::strlen(buffer); i++)
+            {
+                if (buffer[i] == ',')
+                {
+                    buffer[i] = '.';
+                }
+            }
+            MQTT::Packet::Publish(name, buffer);
+        }
+
+        void OnEventSend()
+        {
+            if (need)
+            {
+                if (value.is_good[0])
+                {
+                    SendMeasure("/voltage/a", value.measures[0].voltage);
+                    SendMeasure("/current/a", value.measures[0].current * 1000.0f);
+                }
+
+                if (value.is_good[1])
+                {
+                    SendMeasure("/voltage/b", value.measures[1].voltage);
+                    SendMeasure("/current/b", value.measures[1].current * 1000.0f);
+                }
+
+                if (value.is_good[2])
+                {
+                    SendMeasure("/voltage/c", value.measures[2].voltage);
+                    SendMeasure("/current/c", value.measures[2].current * 1000.0f);
+                }
+
+                need = false;
+            }
+        }
+    }
+}
--- sources/common/Modem/Sender/Measure.h
+++ sources/common/Modem/Sender/Measure.h
@@ -1,82 +1,17 @@
 // 2023/05/26 08:00:46 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 #include "Modem/MQTT.h"
-#include <cstdio>
-#include <cstring>
+#include "Measurer/Measurer.h"
 
 
 namespace Sender
 {
     namespace Measure
     {
-        static FullMeasure value;
-        static bool need = false;
+        void Send(const FullMeasure &meas);
 
-        inline void Send(const FullMeasure &meas)
-        {
-            if (!MQTT::InStateRunning())
-            {
-                return;
-            }
+        void SendMeasure(pchar name, float voltage);
 
-            static TimeMeterMS meter;
-
-            static bool first = true;
-
-            if (meter.ElapsedTime() < 60000 && !first)
-            {
-                return;
-            }
-
-            first = false;
-
-            meter.Reset();
-
-            value = meas;
-
-            need = true;
-
-            MQTT::Send::SendRequest();
-        }
-
-        inline void SendMeasure(pchar name, float voltage)
-        {
-            char buffer[32];
-            std::sprintf(buffer, "%d", (int)(voltage + 0.5f));
-            for (uint i = 0; i < std::strlen(buffer); i++)
-            {
-                if (buffer[i] == ',')
-                {
-                    buffer[i] = '.';
-                }
-            }
-            MQTT::Packet::Publish(name, buffer);
-        }
-
-        inline void OnEventSend()
-        {
-            if (need)
-            {
-                if (value.is_good[0])
-                {
-                    SendMeasure("/voltage/a", value.measures[0].voltage);
-                    SendMeasure("/current/a", value.measures[0].current * 1000.0f);
-                }
-
-                if (value.is_good[1])
-                {
-                    SendMeasure("/voltage/b", value.measures[1].voltage);
-                    SendMeasure("/current/b", value.measures[1].current * 1000.0f);
-                }
-
-                if (value.is_good[2])
-                {
-                    SendMeasure("/voltage/c", value.measures[2].voltage);
-                    SendMeasure("/current/c", value.measures[2].current * 1000.0f);
-                }
-
-                need = false;
-            }
-        }
+        void OnEventSend();
     }
 }
