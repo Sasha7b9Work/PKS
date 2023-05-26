c1116ffcf9d678743f8408c5be21428779566c97
Sasha7b9Work <Phuntik-it@tut.by>
2023-05-26 15:33:53 +0300

    *

--- sources/Device/src/Modem/SIM800.cpp
+++ sources/Device/src/Modem/SIM800.cpp
@@ -172,6 +172,9 @@ void SIM800::Update(pchar answer)
     else if (strcmp(answer, "+CREG: +CRE") == 0)
     {
     }
+    else if (strcmp(answer, "+RDY") == 0)
+    {
+    }
     else
     {
         int i = 0;
