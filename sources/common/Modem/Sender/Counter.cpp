5bfe63611c64410a5261f8b7f4f223292dc7472f
Sasha7b9Work <Phuntik-it@tut.by>
2023-05-26 12:04:01 +0300

    *

---
+++ sources/common/Modem/Sender/Counter.cpp
@@ -0,0 +1,41 @@
+// 2023/05/26 12:02:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Modem/Sender/Counter.h"
+#include "Hardware/Timer.h"
+#include "Modem/MQTT.h"
+#include <cstdio>
+
+
+namespace Sender
+{
+    namespace Counter
+    {
+        static int value = 0;
+        static bool need = false;
+        static TimeMeterMS meter;
+
+        void OnStateRunning()
+        {
+            if (meter.IsFinished())
+            {
+                need = true;
+                MQTT::Send::SendRequest();
+            }
+        }
+
+        void OnEventSend()
+        {
+            if (need)
+            {
+                need = false;
+
+                char buffer[32];
+                std::sprintf(buffer, "%d", ++value);
+
+                MQTT::Packet::Publish("/counter", buffer);
+
+                meter.SetResponseTime(5000);
+            }
+        }
+    }
+}
--- sources/common/Modem/Sender/Counter.h
+++ sources/common/Modem/Sender/Counter.h
@@ -1,40 +1,13 @@
 // 2023/05/25 21:46:33 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
-#include "Hardware/Timer.h"
-#include "Modem/MQTT.h"
-#include <cstdio>
 
 
 namespace Sender
 {
     namespace Counter
     {
-        static int value = 0;
-        static bool need = false;
-        static TimeMeterMS meter;
+        void OnStateRunning();
 
-        void OnStateRunning()
-        {
-            if (meter.IsFinished())
-            {
-                need = true;
-                MQTT::Send::SendRequest();
-            }
-        }
-
-        void OnEventSend()
-        {
-            if (need)
-            {
-                need = false;
-
-                char buffer[32];
-                std::sprintf(buffer, "%d", ++value);
-
-                MQTT::Packet::Publish("/counter", buffer);
-
-                meter.SetResponseTime(5000);
-            }
-        }
+        void OnEventSend();
     }
 }
