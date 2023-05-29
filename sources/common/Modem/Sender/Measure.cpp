080618f85fd44693c07a2378d0971aafcc235111
Sasha7b9Work <Phuntik-it@tut.by>
2023-05-29 10:00:34 +0300

    *

--- sources/common/Modem/Sender/Measure.cpp
+++ sources/common/Modem/Sender/Measure.cpp
@@ -60,20 +60,20 @@ namespace Sender
             {
                 if (value.is_good[0])
                 {
-                    SendMeasure("/base/state/voltage_a", value.measures[0].voltage);
-//                    SendMeasure("/base/state/current_a", value.measures[0].current * 1000.0f);
+                    SendMeasure("base/state/voltage_a", value.measures[0].voltage);
+//                    SendMeasure("base/state/current_a", value.measures[0].current * 1000.0f);
                 }
 
 //                if (value.is_good[1])
 //                {
-//                    SendMeasure("/base/state/voltage_b", value.measures[1].voltage);
-//                    SendMeasure("/base/state/current_b", value.measures[1].current * 1000.0f);
+//                    SendMeasure("base/state/voltage_b", value.measures[1].voltage);
+//                    SendMeasure("base/state/current_b", value.measures[1].current * 1000.0f);
 //                }
 //
 //                if (value.is_good[2])
 //                {
-//                    SendMeasure("/base/state/voltage_c", value.measures[2].voltage);
-//                    SendMeasure("/base/state/current_c", value.measures[2].current * 1000.0f);
+//                    SendMeasure("base/state/voltage_c", value.measures[2].voltage);
+//                    SendMeasure("base/state/current_c", value.measures[2].current * 1000.0f);
 //                }
 
                 need = false;
