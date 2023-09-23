77674950976406378ae5c7845e0341837f1cb4e1
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2023-09-23 22:43:33 +0300

    *

--- sources/Device/src/Storage/Storage.cpp
+++ sources/Device/src/Storage/Storage.cpp
@@ -17,3 +17,72 @@ void Storage::Update()
 
     Contactors::Update(Measurer::Measure5Sec());
 }
+
+
+int Measurements::Flags::NumBitKM(Phase::E phase, int num) const
+{
+    return num + (int)phase * 9;
+}
+
+
+int Measurements::Flags::NumBItGP(Phase::E phase) const
+{
+    return 28 + (int)phase;
+}
+
+
+void Measurements::Flags::SetKM(Phase::E phase, int num, bool state)
+{
+    if (state)
+    {
+        _SET_BIT(bits, NumBitKM(phase, num));
+    }
+    else
+    {
+        _CLEAR_BIT(bits, NumBitKM(phase, num));
+    }
+}
+
+
+bool Measurements::Flags::GetKM(Phase::E phase, int num) const
+{
+    return (_GET_BIT(bits, NumBitKM(phase, num)) != 0);
+}
+
+
+void Measurements::Flags::SetGP(Phase::E phase, bool state)
+{
+    if (state)
+    {
+        _SET_BIT(bits, NumBItGP(phase));
+    }
+    else
+    {
+        _CLEAR_BIT(bits, NumBItGP(phase));
+    }
+}
+
+
+bool Measurements::Flags::GetGP(Phase::E phase) const
+{
+    return (_GET_BIT(bits, NumBItGP(phase)) != 0);
+}
+
+
+void Measurements::Flags::Set100V(bool state)
+{
+    if (state)
+    {
+        _SET_BIT(bits, 27);
+    }
+    else
+    {
+        _CLEAR_BIT(bits, 27);
+    }
+}
+
+
+bool Measurements::Flags::Get100V() const
+{
+    return _GET_BIT(bits, 27) != 0;
+}
--- sources/Device/src/Storage/Storage.h
+++ sources/Device/src/Storage/Storage.h
@@ -7,6 +7,35 @@ struct Measurements
 {
     uint number;
     FullMeasure measure;
+
+    struct Flags
+    {
+        //  0 - kma1
+        //  8 - kma9
+        //  9 - kmb1
+        // 17 - kmb9
+        // 18 - kmc1
+        // 26 - kmc9
+        // 27 - 100V
+        // 28 - gpA
+        // 29 - gpB
+        // 30 - gpC
+        uint bits;
+
+        // num - от нуля до 8 (KM1...KM9)
+        void SetKM(Phase::E, int num, bool);
+        bool GetKM(Phase::E, int num) const;
+        void SetGP(Phase::E, bool);
+        bool GetGP(Phase::E) const;
+        void Set100V(bool);
+        bool Get100V() const;
+
+    private:
+
+        int NumBitKM(Phase::E, int) const;
+        int NumBItGP(Phase::E) const;
+
+    } flags;
 };
 
 
