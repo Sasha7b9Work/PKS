4220558cc6fd97236740f33530e3272e3270af35
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-01-16 22:27:06 +0300

    *

--- sources/common/Modem/SCPI/NetSCPI.cpp
+++ sources/common/Modem/SCPI/NetSCPI.cpp
@@ -3,7 +3,140 @@
 #include "Modem/SCPI/SCPI.h"
 
 
+namespace SCPI
+{
+    namespace NET
+    {
+        static pchar Empty(pchar);
+        static pchar All(pchar);
+        static pchar Auto(pchar);
+        static pchar Cur(pchar);
+        static pchar Cos(pchar);
+        static pchar C(pchar);
+        static pchar I(pchar);
+        static pchar Va(pchar);
+        static pchar Ver(pchar);
+        static pchar Vin(pchar);
+        static pchar V(pchar);
+        static pchar Reset(pchar);
+
+        static StructSCPI commands[] =
+        {
+            { "",      Empty },
+            { "ALL",   All },
+            { "AUTO",  Auto },
+            { "CUR",   Cur },
+            { "COS",   Cos },
+            { "C",     C },
+            { "I",     I },
+            { "VA",    Va },
+            { "VER",   Ver },
+            { "VIN",   Vin },
+            { "V",     V },
+            { "RESET", Reset },
+            { nullptr, nullptr }
+        };
+    }
+}
+
+
 pchar SCPI::NET::Net(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Empty(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::All(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Auto(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Cur(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Cos(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::C(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::I(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Va(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Ver(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Vin(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::V(pchar)
+{
+    SendUnsupportedCommand();
+
+    return nullptr;
+}
+
+
+pchar SCPI::NET::Reset(pchar)
 {
     SendUnsupportedCommand();
 
