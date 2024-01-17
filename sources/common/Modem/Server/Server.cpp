d0be764e8092c485feb595620d0fc584890c65c5
Sasha7b9Work <Phuntik-it@tut.by>
2024-01-17 09:44:25 +0300

    *

--- sources/common/Modem/Server/Server.cpp
+++ sources/common/Modem/Server/Server.cpp
@@ -1,7 +1,10 @@
 // 2024/01/17 08:33:13 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Modem/Server/Server.h"
+#include "Modem/SIM800.h"
 #include <cstdarg>
+#include <cstdio>
+#include <cstring>
 
 
 namespace Server
@@ -11,27 +14,85 @@ namespace Server
         enum E
         {
             IDLE,
-
+            WAIT_PERMISSION_FOR_SEND,   // Ожидаем разрешение на передачу данных
+            WAIT_CONFIRM_FOR_SEND       // Ожидаем подтверждение отправки данных
         };
+
+        static void Set(E v)
+        {
+            current = v;
+        }
+
+        static E Current() { return current; }
+
+    private:
+
+        static E current;
     };
 
-    namespace Buffer
+    State::E State::current = State::IDLE;
+
+    struct Buffer
     {
-        void Append(pchar);
+        static void Append(pchar);
 
+        static pchar GetData()
+        {
+            return buffer;
+        }
 
-    }
+        static void Clear()
+        {
+            pointer = 0;
+        }
 
-    // Признак того, что нужен запрос на передачу данных
-    static bool need_send = true;
+        static int Size()
+        {
+            return pointer;
+        }
+
+    private:
+
+        static const int SIZE = 512;
+        static char buffer[SIZE];
+        static int pointer;
+    };
+
+    int Buffer::pointer = 0;
+    char Buffer::buffer[Buffer::SIZE];
 }
 
 
 void Server::Update(pchar answer)
 {
-    if (answer[0] == '>')
+    switch (State::Current())
     {
+    case State::IDLE:
+        if (Buffer::Size())
+        {
+            SIM800::Transmit::With0D("AT+CIPSEND");
 
+            State::Set(State::WAIT_PERMISSION_FOR_SEND);
+        }
+        break;
+
+    case State::WAIT_PERMISSION_FOR_SEND:
+        if (answer[0] == '>')
+        {
+            SIM800::Transmit::RAW(Buffer::GetData());
+            SIM800::Transmit::UINT8(0x1A);
+            Buffer::Clear();
+
+            State::Set(State::WAIT_CONFIRM_FOR_SEND);
+        }
+        break;
+
+    case State::WAIT_CONFIRM_FOR_SEND:
+        if (std::strcmp(answer, "SEND OK") == 0)
+        {
+            State::Set(State::IDLE);
+        }
+        break;
     }
 }
 
@@ -44,7 +105,7 @@ bool Server::IsConnected()
 
 void Server::Reset()
 {
-
+    Buffer::Clear();
 }
 
 
@@ -56,7 +117,18 @@ void Server::Send(char *format, ...)
     va_start(args, format);
     std::vsprintf(message, format, args);
 
-    Buffer::Append(message);
+    std::strcat(message, "\0x0D");
 
-    need_send = true;
+    Buffer::Append(message);
+}
+
+
+void Server::Buffer::Append(pchar data)
+{
+    if (pointer + std::strlen(data) < SIZE)
+    {
+        std::strcat(buffer, data);
+
+        pointer += std::strlen(data);
+    }
 }
